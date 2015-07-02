ebec258750818a600fcf9fba13fd67eb94f49dbe
wylkerxpz <wylkerxpz@gmail.com>
2015-07-02 19:13:50 -0300

    Acrescimo de listview correto

--- App/MyApplication/app/src/main/java/br/com/nwms/myapplication/MainActivity.java
+++ App/MyApplication/app/src/main/java/br/com/nwms/myapplication/MainActivity.java
@@ -1,11 +1,23 @@
 package br.com.nwms.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener{
@@ -13,6 +25,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     UserLocalStore userLocalStore;
     EditText etName, etUsername;
     Button bLogout;
+
+    MyCustomAdapter dataAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +40,11 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         bLogout.setOnClickListener(this);
 
         userLocalStore = new UserLocalStore(this);
+
+        //Generate list View from ArrayList
+        displayListView();
+
+        checkButtonClick();
     }
 
     @Override
@@ -61,5 +80,132 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         User user = userLocalStore.getLoggedInUser();
         etUsername.setText(user.username);
         etName.setText(user.name);
+    }
+
+    private void displayListView() {
+
+        //Array list of countries
+        ArrayList<Country> countryList = new ArrayList<Country>();
+        Country country = new Country("AFG","Afghanistan",false);
+        countryList.add(country);
+        country = new Country("ALB","Albania",true);
+        countryList.add(country);
+        country = new Country("DZA","Algeria",false);
+        countryList.add(country);
+        country = new Country("ASM","American Samoa",true);
+        countryList.add(country);
+        country = new Country("AND","Andorra",true);
+        countryList.add(country);
+        country = new Country("AGO","Angola",false);
+        countryList.add(country);
+        country = new Country("AIA","Anguilla",false);
+        countryList.add(country);
+
+        //create an ArrayAdaptar from the String Array
+        dataAdapter = new MyCustomAdapter(this,
+                R.layout.disciplinas_info, countryList);
+        ListView listView = (ListView) findViewById(R.id.listView1);
+        // Assign adapter to ListView
+        listView.setAdapter(dataAdapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // When clicked, show a toast with the TextView text
+                Country country = (Country) parent.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),
+                        "Clicked on Row: " + country.getName(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private class MyCustomAdapter extends ArrayAdapter<Country> {
+
+        private ArrayList<Country> countryList;
+
+        public MyCustomAdapter(Context context, int textViewResourceId,
+                               ArrayList<Country> countryList) {
+            super(context, textViewResourceId, countryList);
+            this.countryList = new ArrayList<Country>();
+            this.countryList.addAll(countryList);
+        }
+
+        private class ViewHolder {
+            TextView code;
+            CheckBox name;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+            Log.v("ConvertView", String.valueOf(position));
+
+            if (convertView == null) {
+                LayoutInflater vi = (LayoutInflater)getSystemService(
+                        Context.LAYOUT_INFLATER_SERVICE);
+                convertView = vi.inflate(R.layout.disciplinas_info, null);
+
+                holder = new ViewHolder();
+                holder.code = (TextView) convertView.findViewById(R.id.code);
+                holder.name = (CheckBox) convertView.findViewById(R.id.checkBox1);
+                convertView.setTag(holder);
+
+                holder.name.setOnClickListener( new View.OnClickListener() {
+                    public void onClick(View v) {
+                        CheckBox cb = (CheckBox) v ;
+                        Country country = (Country) cb.getTag();
+                        Toast.makeText(getApplicationContext(),
+                                "Clicked on Checkbox: " + cb.getText() +
+                                        " is " + cb.isChecked(),
+                                Toast.LENGTH_LONG).show();
+                        country.setSelected(cb.isChecked());
+                    }
+                });
+            }
+            else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            Country country = countryList.get(position);
+            holder.code.setText(" (" +  country.getCode() + ")");
+            holder.name.setText(country.getName());
+            holder.name.setChecked(country.isSelected());
+            holder.name.setTag(country);
+
+            return convertView;
+
+        }
+
+    }
+
+    private void checkButtonClick() {
+
+        Button myButton = (Button) findViewById(R.id.findSelected);
+        myButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                StringBuffer responseText = new StringBuffer();
+                responseText.append("The following were selected...\n");
+
+                ArrayList<Country> countryList = dataAdapter.countryList;
+                for (int i = 0; i < countryList.size(); i++) {
+                    Country country = countryList.get(i);
+                    if (country.isSelected()) {
+                        responseText.append("\n" + country.getName());
+                    }
+                }
+
+                Toast.makeText(getApplicationContext(),
+                        responseText, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 }
